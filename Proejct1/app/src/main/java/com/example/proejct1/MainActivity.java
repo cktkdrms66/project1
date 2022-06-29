6cd4cab8a62e561847d81d3a54cb88807a291e52
cktkdrms66 <cktkdrms66@hanyang.ac.kr>
2022-06-29 19:36:37 +0900

    리사이클러뷰 추가

--- Proejct1/app/src/main/java/com/example/proejct1/MainActivity.java
+++ Proejct1/app/src/main/java/com/example/proejct1/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.proejct1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView img;
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        textView1 = findViewById(R.id.textView);
+        textView1 = findViewById(R.id.name);
         button = findViewById(R.id.button111);
         img = findViewById(R.id.iuimg);
+        recyclerView = findViewById(R.id.recylerview);
+
 
         textView1.setText("fjweipfwefpejfopwjfpewfpjewqopfpejowfpfsejfpisefjpsfpj");
 
@@ -34,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
                 img.setImageResource(R.drawable.iu2);
             }
         });
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false)) ; // 상하 스크롤
+
+        Tab1Adapter adapter = new Tab1Adapter();
+
+        ArrayList<String> tests = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            String str = i + "번째 아이템";
+            tests.add(str);
+        }
+
+        adapter.setItems(tests);
+
+        recyclerView.setAdapter(adapter);
+
 
 
     }
