be54c833c6f6ed72d5985dd9f23dd5871ebe7f21
ki-met-hoon <shggm2000@gmail.com>
2022-07-05 13:15:27 +0900

    분반 정보 추가 2

--- Proejct1/app/src/main/java/com/example/proejct1/activity/SplashActivity.java
+++ Proejct1/app/src/main/java/com/example/proejct1/activity/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.example.proejct1.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,15 +18,23 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        ImageView ddabong = findViewById(R.Id.ddabong);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.spin);
+        ddabong.setAnimation(anim);
         moveMain(2);
+
     }
 
     private void moveMain(int sec) {
         new Handler().postDelayed(new Runnable()
         {
+            @SuppressLint("ResourceType")
             @Override
             public void run()
             {
+
                 //new Intent(현재 context, 이동할 activity)
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
@@ -32,6 +44,8 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 1000 * sec); // sec초 정도 딜레이를 준 후 시작
     }
+
+
 
 }
 
